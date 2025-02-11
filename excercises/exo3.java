8f850e1d0599a39f3f8b06a338ae8e4802bcd047
daxyman <danielfelipegg3005@gmail.com>
2025-02-10 19:24:20 -0500

    exo1-3

---
+++ excercises/exo3.java
@@ -0,0 +1,9 @@
+public class exo3 {
+    public static void main(String args[]){
+        String name1 = new String();
+        boolean name2 = true;
+        char name3 = 'a';
+        int name4 = 12;
+        float name5 = 13;
+    }
+}
