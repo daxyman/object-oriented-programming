5f344142bb79225d1cd6db8bf26d736772e58f8b
daxyman <danielfelipegg3005@gmail.com>
2025-02-20 21:20:26 -0500

    exo13Works

--- excercises/exo13.java
+++ excercises/exo13.java
@@ -1,19 +1,29 @@
 public class exo13 {
     public static void main(String[] args){
-        int[] table = new int[100];
+        int[] table = new int[20];
         table = fillTable(table);
         printTable(table);
+        printTable(tableNoDupes(table));
+
     }
 
-    public static int[] removeDupes(int[] table){
+    public static int[] tableNoDupes(int[] table){
+        int[] tab2 = new int[table.length];
+        int tabNewLength = 0;
         for(int i=0; i<table.length; i++){
-            for(int j=i; j<table.length; j++){
-                
+            System.out.println("\nnumber "+(i+1)+ " ");
+            for(int j=i+1; j<table.length; j++){
+                if(table[j]==table[i]){
+                System.out.println("found dupe "+ (tabNewLength+1) + " : " + table[i]);
+                tabNewLength += 1;
+                }
             }
         }
+        System.out.println("the amount of dupes found is: " + tabNewLength);
+        return tab2;
     }
     public static void printTable(int[] table){
-        System.out.print('[');
+        System.out.print("\n[");
         for(int i=0; i<table.length; i++){
             System.out.print(table[i]);
             if(i == table.length-1){
@@ -21,7 +31,7 @@ public class exo13 {
             }
             System.out.print(',');
         }
-        System.out.print(']');
+        System.out.println(']');
     }
 
     public static int[] fillTable(int[] table){
