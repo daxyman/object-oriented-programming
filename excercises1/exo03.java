8eb3712619403c2ed8b7f361230b707c3c2deda6
daxyman <danielfelipegg3005@gmail.com>
2025-03-31 19:53:38 -0400

    merging

--- excercises1/exo03.java
+++ excercises1/exo03.java
@@ -1,9 +1,9 @@
 public class exo03 {
     public static void main(String args[]){
-        String name1 = new String();
-        boolean name2 = true;
-        char name3 = 'a';
-        int name4 = 12;
-        float name5 = 13;
+        // String name1 = new String();
+        // boolean name2 = true;
+        // char name3 = 'a';
+        // int name4 = 12;
+        // float name5 = 13;
     }
 }
--- excercises1/exo05.java
+++ excercises1/exo05.java
@@ -1,6 +1,7 @@
 public class exo05 {
     public static void main(String args[]){
         int demo = 0;
+        System.out.println(demo);
         demo = 8;
         System.out.println(demo);
         demo = demo+100;
